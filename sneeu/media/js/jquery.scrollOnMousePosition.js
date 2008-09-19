534480ab00da1c5d596137bbc09b2fe3777f00ca
john <john@6d9696ac-a29a-4b77-b74e-909eb48be701>
2008-09-19 18:28:06 +0000

    Adds the chars page and a little more work on ebooks.
    git-svn-id: file:///repos/sneeu/trunk@47 6d9696ac-a29a-4b77-b74e-909eb48be701

--- sneeu/media/js/jquery.scrollOnMousePosition.js
+++ sneeu/media/js/jquery.scrollOnMousePosition.js
@@ -22,13 +22,54 @@
 
 	$.fn.scrollOnMousePosition = function() {
 		var self = $(this);
+		var infoPanel = $('<div id="scrollOnMouseMove_infoPanel"></div>');
+		infoPanel.css('height', '320px').css('position', 'fixed').css('top', '50%').css('left', '50%').css('margin', '-160px 0 0 -160px').css('width', '320px').css('opacity', '0.5').css('z-index', '9999').css('background', '#000 url(/media/img/up.png) no-repeat top left');
+		infoPanel.addClass('hidden');
+		self.append(infoPanel);
+		infoPanel.hide();
 
 		var left = self.clientLeft;
-		console.log(left);
-		// self.css('overflow', 'hidden').css('position', 'absolute').css('top', '0px');
-		// 
-		// self.mousemove(function(ev) {
-		// 	self.css('top', ev.clientY / window.innerHeight * self.height() * -1);
-		// });
+
+		self.css('overflow', 'hidden').css('position', 'relative').css('top', '0px');
+
+		function theScroll(jqElem, n) {
+			console.log(n, jqElem.halt);
+			if (!jqElem.halt) {
+				var newTop = parseInt(jqElem.css('top'));
+				// console.log(newTop, newTop + n);
+				jqElem.animate({scrollTop: newTop + n}, 100, function() {
+					jqElem.halt = false;
+					theScroll(jqElem, n);
+				});
+			 	jqElem.halt = true;
+			}
+		}
+
+		self.mousemove(function(ev) {
+			if (window.innerHeight - ev.clientY < 120) {
+				if (infoPanel.hasClass('hidden')) {
+					infoPanel.removeClass('hidden');
+					infoPanel.fadeIn(200);
+				}
+				infoPanel.css('background-image', 'url(/media/img/down.png)');
+				theScroll(self, 100);
+				// while (!infoPanel.hasClass('hidden')) {
+					// self.animate({scrollTop: 100});
+				// }
+			} else if (ev.clientY < 120) {
+				if (infoPanel.hasClass('hidden')) {
+					infoPanel.removeClass('hidden');
+					infoPanel.fadeIn(200);
+				}
+				infoPanel.css('background-image', 'url(/media/img/up.png)');
+				theScroll(self, -100);
+				// }
+				//ev.clientY / window.innerHeight * self.height() * -1;
+			} else {
+				self.halt = true;
+				infoPanel.fadeOut(200);
+				infoPanel.addClass('hidden');
+			}
+		});
 	};
 })(jQuery);
